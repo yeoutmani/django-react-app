68b4ad17e4917264d3b7b3343837b0f11df4750d
EL OUTMANI YASSINE <85106701+yeoutmani@users.noreply.github.com>
2024-01-23 18:27:17 +0100

    Send Data from Frontend to Django Backend

--- frontend/src/components/Create.js
+++ frontend/src/components/Create.js
@@ -6,12 +6,33 @@ import MyTextField from './forms/MyTextFiled'
 import MySelectField from './forms/MySelectField'
 import MyMultiLineField from './forms/MyMultiLine'
 import { useForm } from 'react-hook-form'
-
+import AxiosInstance from './Axios'
+import Dayjs from 'dayjs'
+import { useNavigate } from 'react-router-dom'
 
 const Create = () => {
-
-  const { handleSubmit, reset, setValue, control } = useForm()
-  const submission = (data) => console.log(data)
+  const navigate = useNavigate()
+  const defaultValues = {
+    name: '',
+    comments: '',
+    status: ''
+  }
+  const { handleSubmit, reset, setValue, control } = useForm({ defaultValues: defaultValues })
+  const submission = (data) => {
+    const StartDate = Dayjs(data.start_date["$d"]).format("YYYY-MM-DD")
+    const EndDate = Dayjs(data.end_date["$d"]).format("YYYY-MM-DD")
+    AxiosInstance.post(`project/`, {
+      name: data.name,
+      name: data.name,
+      status: data.status,
+      comments: data.comments,
+      start_date: StartDate,
+      end_date: EndDate,
+    }).then((res) => {
+      navigate(`/`)
+    })
+    console.log(data)
+  }
 
   return (
     <div>
