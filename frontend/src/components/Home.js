46ca5ab0ef85a48da8898acdb883ea4e5c595212
EL OUTMANI YASSINE <85106701+yeoutmani@users.noreply.github.com>
2024-02-01 18:09:56 +0100

    docker django and react

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import AxiosInstance from './Axios'
 
 const Home = () => {
+  AxiosInstance.get(`project/`).then((res) => {
+    console.log('res', res)
+  })
   return (
     <div>Home</div>
   )
